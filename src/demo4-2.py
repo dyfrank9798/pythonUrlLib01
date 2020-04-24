0af5ca0b486edf9bfe19526dd4192e39664c5aa2
dyfrank9798 <936589714@qq.com>
2020-04-24 15:41:47 +0800

    4-2

--- src/demo4-2.py
+++ src/demo4-2.py
@@ -1,3 +1,18 @@
 #多进程爬虫(不同于多线程)
 
-11dadad
+import time
+
+
+def job(t):
+    print('Start job ', t)
+    time.sleep(t)               # wait for "t" seconds
+    print('Job ', t, ' takes ', t, ' s')
+
+
+def main():
+    [job(t) for t in range(1, 3)]
+
+
+t1 = time.time()
+main()
+print("NO async total time : ", time.time() - t1)
