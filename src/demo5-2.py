b710988a831351faa61214c32ccc4bf65b16ca3c
dyfrank9798 <936589714@qq.com>
2020-04-28 11:26:35 +0800

    by workLaptop

--- src/demo5-2.py
+++ src/demo5-2.py
@@ -1,19 +1,24 @@
-from selenium.webdriver.chrome.options import Options
-from selenium import webdriver
-chrome_options = Options()
-chrome_options.add_argument("--headless")       # define headless
+import scrapy
 
-# add the option when creating driver
-driver = webdriver.Chrome(chrome_options=chrome_options)#不打开实体浏览器
-driver.get("https://morvanzhou.github.io/")
-driver.find_element_by_xpath(u"//img[@alt='强化学习 (Reinforcement Learning)']").click()
-driver.find_element_by_link_text("About").click()
-driver.find_element_by_link_text(u"赞助").click()
-driver.find_element_by_link_text(u"教程 ▾").click()
-driver.find_element_by_link_text(u"数据处理 ▾").click()
-driver.find_element_by_link_text(u"网页爬虫").click()
 
-html = driver.page_source           # get html
-driver.get_screenshot_as_file("./img/sreenshot2.png")
-driver.close()
-print(html[:200])
+class MofanSpider(scrapy.Spider):
+    name = "mofan"
+    start_urls = [
+        'https://morvanzhou.github.io/',
+    ]
+    # unseen = set()
+    # seen = set()      # we don't need these two as scrapy will deal with them automatically
+
+    def parse(self, response):
+        yield {     # return some results
+            'title': response.css('h1::text').extract_first(default='Missing').strip().replace('"', ""),
+            'url': response.url,
+        }
+
+        urls = response.css('a::attr(href)').re(r'^/.+?/$')     # find all sub urls
+        for url in urls:
+            yield response.follow(url, callback=self.parse)     # it will filter duplication automatically
+
+
+# lastly, run this in terminal
+# scrapy runspider 5-2-scrapy.py -o res.json
